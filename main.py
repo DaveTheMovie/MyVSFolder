af60cdef0438d68ef8448756f7e0d7f443e6330c
David Romoff <DaveTheMovie@gmail.com>
2022-04-09 17:54:07 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+
+
+class GameWidget(Widget):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Rectangle(pos= (0,0),size = (100,100))
+
+
+class MyApp(App):
+    def build(self):
+        return Widget()
+    
+if __name__ == "__main___":
+    app = MyApp()
+    app.run()
